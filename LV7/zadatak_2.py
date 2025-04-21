c9991c591a970ff24a6cc9bf86ca984d24e87485
eivelic <166445861+eivelic@users.noreply.github.com>
2025-04-21 12:29:45 +0200

    Add zadatak_2.py

--- LV7/zadatak_2.py
+++ LV7/zadatak_2.py
@@ -4,7 +4,7 @@ import matplotlib.image as Image
 from sklearn.cluster import KMeans
 
 # ucitaj sliku
-img = Image.imread("imgs\\imgs\\test_1.jpg")
+img = Image.imread("test_2.jpg")
 
 # prikazi originalnu sliku
 plt.figure()
@@ -33,95 +33,55 @@ print(f"Broj različitih boja u slici: {unique_colors.shape[0]}")
 
 # 2. Primijenite algoritam K srednjih vrijednosti koji ce pronaći grupe u RGB vrijednostima
 # elemenata originalne slike.
-n_clusters = 5
-kmeans = KMeans(n_clusters=5, random_state=42)
+
+K=5
+kmeans=KMeans(n_clusters=K, init='k-means++', random_state=0)
 kmeans.fit(img_array)
 
-labels = kmeans.predict(img_array)
+centroids=kmeans.cluster_centers_
+labels=kmeans.labels_
 
-cluster_centers = kmeans.cluster_centers_
-
-img_array_aprox = cluster_centers[labels]
-img_array_aprox = np.reshape(img_array_aprox, (w, h, d))
-
-plt.figure()
-plt.title("Originalna slika")
-plt.imshow(img)
-plt.tight_layout()
-plt.show()
-
-plt.figure()
-plt.title(f"Slika s {n_clusters} klastera")
-plt.imshow(img_array_aprox)
-plt.tight_layout()
-plt.show()
+print('Centroidi boja:')
+print(centroids)
 
 # 3. Vrijednost svakog elementa slike originalne slike zamijeni s njemu pripadajućim centrom.
 
+img_array_aprox=centroids[labels]
+img_approx=np.reshape(img_array_aprox,(w,h,d))
+
 plt.figure()
-plt.title(f"Slika s K-Means centrom za svaki piksel ({n_clusters} klastera)")
-plt.imshow(img_array_aprox)
+plt.title(f"Kvantizirana slika (K = {K})")
+plt.imshow(img_approx)
 plt.tight_layout()
 plt.show()
 
 # 4. Usporedite dobivenu sliku s originalnom. Mijenjate broj grupa K. 
 # Komentirajte dobivene rezultate.
+
+for K in [2, 4, 8, 16, 32]:
+    kmeans = KMeans(n_clusters=K, init='random',random_state=0)
+    kmeans.fit(img_array)
+    centroids = kmeans.cluster_centers_
+    labels = kmeans.labels_
+    img_aprox = np.reshape(centroids[labels], (w, h, d))
+
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    axs[0].imshow(img)
+    axs[0].set_title("Original")
+    axs[0].axis("off")
+    axs[1].imshow(img_aprox)
+    axs[1].set_title(f"K = {K}")
+    axs[1].axis("off")
+    plt.tight_layout()
+    plt.show()
+
 # Komentar:
 # S povećanjem broja klastera (K), kvaliteta rekonstrukcije slike se poboljšava jer se broj boja povećava, što omogućuje precizniju reprodukciju originalne slike.
 # Ipak, rekonstrukcija s K-Means uvijek podrazumijeva gubitak određenih detalja jer ne možemo točno replicirati sve boje prisutne u originalu.
 
 # 5. Primijenite postupak i na ostale dostupne slike.
 
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as Image
-from sklearn.cluster import KMeans
-import os
-
-def apply_kmeans_and_plot(img, n_clusters):
-    img = img.astype(np.float64) / 255
-
-    img = np.clip(img, 0, 1)
-
-    w, h, d = img.shape
-    img_array = np.reshape(img, (w*h, d))
-
-    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
-    kmeans.fit(img_array)
-    
-    labels = kmeans.predict(img_array)
-    
-    cluster_centers = kmeans.cluster_centers_
-
-    img_array_aprox = cluster_centers[labels]
-    img_array_aprox = np.reshape(img_array_aprox, (w, h, d))
-
-    plt.figure(figsize=(10, 5))
-
-    plt.subplot(1, 2, 1)
-    plt.title("Originalna slika")
-    plt.imshow(img)
-    plt.axis('off')
-
-    plt.subplot(1, 2, 2)
-    plt.title(f"Slika s {n_clusters} klastera")
-    plt.imshow(img_array_aprox)
-    plt.axis('off')
-    
-    plt.tight_layout()
-    plt.show()
-
-def process_images_in_directory(directory, n_clusters=5):
-    for filename in os.listdir(directory):
-        if filename.endswith(".jpg") or filename.endswith(".png"):
-            img = Image.imread(os.path.join(directory, filename))
-
-            print(f"Obrađujem sliku: {filename}")
-            apply_kmeans_and_plot(img, n_clusters)
-
-image_directory = "imgs\\imgs\\"
-
-process_images_in_directory(image_directory, n_clusters=5)
+# Komentar: Samo mijenjamo sliku u putanji u prethodnom zadatku.
 
 # 6. Grafički prikažite ovisnost J o broju grupa K. Koristite atribut inertia objekta klase KMeans.
 # Možete li uočiti lakat koji upućuje na optimalni broj grupa?
@@ -149,7 +109,7 @@ def plot_inertia_vs_k(img, max_k=10):
     plt.tight_layout()
     plt.show()
 
-img = Image.imread("imgs\\imgs\\test_1.jpg")
+img = Image.imread("test_2.jpg")
 
 plot_inertia_vs_k(img, max_k=10)
 
@@ -181,7 +141,7 @@ def show_binary_images_for_clusters(img, n_clusters):
     plt.tight_layout()
     plt.show()
 
-img = Image.imread("imgs\\imgs\\test_1.jpg")
+img = Image.imread("test_2.jpg")
 
 n_clusters = 5
 show_binary_images_for_clusters(img, n_clusters)
